f78916b0d8e830a58d226b8adb02aedb5599b5f7
duydo8 <duydogch123456@gmail.com>
2022-09-16 16:04:51 +0700

    fix cors allows all

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"Adam-backend-go/initializers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func init() {
@@ -14,16 +13,6 @@ func init() {
 }
 func main() {
 	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+	r.Use(cors.Default())
 	r.Run()
 }
